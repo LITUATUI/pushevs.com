5a2ebe2380d667274465fdc0a5a1958bb4a31383
LITUATUI <49299328+LITUATUI@users.noreply.github.com>
2025-04-02 20:05:37 +0100

    Updates

--- assets/hb/modules/custom/js/index.ts
+++ assets/hb/modules/custom/js/index.ts
@@ -1,1 +1,17 @@
 // This script will be compiled into the JS bundle automatically.
+
+// Show alternative content when adblock is present 
+
+document.addEventListener("DOMContentLoaded", function () {
+    let adScript = document.createElement("script");
+    adScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+    
+    adScript.onerror = function () {
+      let adBlockMessage = document.getElementById("alternative-banner");
+      if (adBlockMessage) {
+        adBlockMessage.style.display = "block";
+      }
+    };
+  
+    document.body.appendChild(adScript);
+  });  
