476fe5431913253e005958691e8ef3c56467b9ce
LITUATUI <49299328+LITUATUI@users.noreply.github.com>
2025-04-02 20:24:57 +0100

    Updates

--- assets/hb/modules/custom/js/index.ts
+++ assets/hb/modules/custom/js/index.ts
@@ -5,19 +5,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   let adScript = document.createElement("script");
   adScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-
+  
   adScript.onerror = function () {
-    console.log("AdBlock detected! Showing alternative ads...");
-    
-    // Find all blocked AdSense ads and show alternatives
-    let ads = document.querySelectorAll(".adsbygoogle");
-    ads.forEach((ad, index) => {
-      let altAd = document.getElementById(`alternative-banner-${index + 1}`);
-      if (altAd) {
-        altAd.style.display = "block";
-        ad.style.display = "none"; // Hide blocked AdSense ad
-      }
-    });
+    let alternativeAd = document.getElementById("alternative-banner");
+    if (alternativeAd) {
+      alternativeAd.style.display = "block";
+    }
   };
 
   document.body.appendChild(adScript);
